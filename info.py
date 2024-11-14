e71befba514ac1585df1426e82c4bc55f20433ec
Itzmecp <121564783+Cpmodzyt@users.noreply.github.com>
2024-11-14 10:07:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -119,7 +119,7 @@ if len(BIN_CHANNEL) == 0:
     exit()
 else:
     BIN_CHANNEL = int(BIN_CHANNEL)
-URL = environ.get("URL", "184.174.33.24")
+URL = environ.get("URL", "184.174.33.24:8888")
 if len(URL) == 0:
     print('Error - URL is missing, exiting now')
     exit()
